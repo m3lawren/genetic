0117165ad16d3caa75112570b724a353fa0d794a
Matt Lawrence <m3lawren@uwaterloo.ca>
2008-12-21 18:37:48 -0500

    Fleshing out Polygon a bit.
    Signed-off-by: Matt Lawrence <m3lawren@uwaterloo.ca>

--- polygon.cc
+++ polygon.cc
@@ -1,7 +1,24 @@
 #include <polygon.h>
 
-Polygon::Polygon() {
+Polygon::Polygon() : _num(0), _x(NULL), _y(NULL) {
+	_colour.r = _colour.g = _colour.b = _colour.a = 0;	
 }
 
 Polygon::~Polygon() {
+}
+
+size_t Polygon::num() const {
+	return _num;
+}
+
+const uint32_t* Polygon::x() const {
+	return _x;
+}
+
+const uint32_t* Polygon::y() const {
+	return _y;
+}
+
+const struct Colour& Polygon::colour() const {
+	return _colour;
 }
--- polygon.h
+++ polygon.h
@@ -1,14 +1,34 @@
 #ifndef _POLYGON_H_
 #define _POLYGON_H_
 
+#include <stdint.h>
+#include <cstdlib>
+
+struct Colour {
+	uint8_t r;
+	uint8_t g;
+	uint8_t b;
+	uint8_t a;
+};
+
 class Polygon {
 	public:
 		Polygon();
 		~Polygon();
 
+		size_t num() const;
+		const uint32_t* x() const;
+		const uint32_t* y() const;
+		const struct Colour& colour() const;
+
 	private:
 		Polygon(const Polygon&);
 		Polygon& operator=(const Polygon&);
+
+		size_t _num;
+		uint32_t* _x;
+		uint32_t* _y;
+		struct Colour _colour;
 };
 
 #endif
