9d4ec0342cf604635425fd758aa4ae3b5d9c01da
Matt Lawrence <m3lawren@uwaterloo.ca>
2008-12-21 19:07:16 -0500

    Revert "Moving polygon's assignment operator into private for now, not sure if it'll be needed."
    This reverts commit 3c7df0f317f2c9829d98f028c20f55b9821b4ec8.

--- polygon.h
+++ polygon.h
@@ -18,14 +18,14 @@ class Polygon {
 
 		~Polygon();
 
+		Polygon& operator=(const Polygon&);
+
 		size_t num() const;
 		const uint32_t* x() const;
 		const uint32_t* y() const;
 		const struct Colour& colour() const;
 
 	private:
-		Polygon& operator=(const Polygon&);
-
 		size_t _cap;
 		size_t _num;
 		uint32_t* _x;
